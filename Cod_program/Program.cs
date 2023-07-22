6f4e4bbc450ec91b81bb47ddd98456b1480f0f2f
Anastasiya <Fankovskaya-asya@mail.ru>
2023-07-22 19:52:49 +0300

    написали код

---
+++ Cod_program/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+
+string[] ArrayString()
+{
+    Console.Write("Введите элементы массива через запятую: ");
+    return Console.ReadLine()!.Replace("\"", "").Split(", ").ToArray();
+}
+
+int Sizearray2(string[] array, int n)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] SecondArray(string[] array, int n)
+{
+    string[] result = new string[Sizearray2(array, n)];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= n)
+        {
+            result[j] =  array[i];
+            j++;           
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"]");
+    Console.WriteLine();
+}
+
+
+string[] array1 = ArrayString();
+PrintArray(array1);
+string[] array2 = SecondArray(array1, 3);
+PrintArray(array2);
+
